370d7dc5434071cd7c581dcc24b80b0999e0c5e8
Tanishak10789 <tanishak9252@gmail.com>
2024-11-16 16:37:53 +0530

    fifth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react'; // Ensure React and useState are imported
+import './App.css'; // Import your CSS file
+import Navbar from './components/shared/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello World</h1>
+    <Navbar/>
+      <h1 className="text-red-500">hello world</h1> {/* Example Tailwind class */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
